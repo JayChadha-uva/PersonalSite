54d4f62cd6dd1789cacc53d5a2b5249ecdc7d6a5
JayChadha-uva <89432060+JayChadha-uva@users.noreply.github.com>
2022-12-15 15:03:44 -0500

    Initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,25 @@
+export default function Index() {
+  return (
+    
+<nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+  <div className="grid grid-cols-3">
+  <div className="self-center text-xl font-semibold whitespace-nowrap dark:text-white my-3 ml-5">Jay Chadha</div>
+  <div className="flex justify-center items-center" id="navbar-sticky">
+  <ul className=" flex flex-row gap-x-5 text-md font-medium border-0 bg-white dark:bg-gray-900 dark:border-gray-700">
+      <li>
+        <a href="#" className="text-white hover:bg-transparent text-blue-700 p-0 dark:text-white" aria-current="page">About</a>
+      </li>
+      <li>
+        <a href="#" className="hover:bg-transparent hover:text-blue-700 p-0 dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:hover:bg-transparent dark:border-gray-700">Portfolio</a>
+      </li>
+      <li>
+        <a href="#" className="hover:bg-transparent hover:text-blue-700 p-0 dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+      </li>
+    </ul>
+    </div>  
+  </div>
+  <div></div>
+</nav>
+
+  );
+}
